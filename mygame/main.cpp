e736981b26c246411fc3a6df56cc51a06f23c749
MKurshakova <mariia.kurshakova@gmail.com>
2024-05-07 21:28:13 +0200

    background

--- mygame/main.cpp
+++ mygame/main.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include "time_counter.h"
 
-void main_loop(SDL_Renderer *renderer, Time_Counter &time_counter) {
+void main_loop(SDL_Renderer *renderer, Time_Counter &time_counter, SDL_Texture* background_texture) {
     while (true) {
         SDL_Event event;
         if (SDL_PollEvent(&event) && (SDL_QUIT==event.type || (SDL_KEYDOWN==event.type && SDLK_ESCAPE==event.key.keysym.sym)))
             break;
         SDL_RenderClear(renderer); // re-draw the window
+        SDL_RenderCopy(renderer, background_texture, NULL, NULL);
         time_counter.draw();
         SDL_RenderPresent(renderer);
     }
@@ -27,11 +28,29 @@ int main(int argc, char *argv[])
         return -1;
     }
     SDL_SetWindowTitle(window, "Eclipse game");
-    SDL_SetRenderDrawColor(renderer, 53, 28, 117, 255);
+
+    SDL_Surface *background_surface = SDL_LoadBMP("assets/background.bmp");
+    if (!background_surface) {
+        std::cerr << "Failed to load background image: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+    SDL_Texture *background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
+    SDL_FreeSurface(background_surface);
+    if (!background_texture) {
+        std::cerr << "Failed to create background texture: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
     Time_Counter time_counter(renderer);
-    main_loop(renderer, time_counter);
+    main_loop(renderer, time_counter, background_texture);
 
+    SDL_DestroyTexture(background_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
